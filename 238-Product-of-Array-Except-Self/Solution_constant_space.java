a27b7d7c912d5d524a409366016a9753b43c40ef
kevinhjbj <kevinhj.bj@gmail.com>
2016-02-07 13:49:10 -0800

    Changes to be committed: 	new file:   238-Product-of-Array-Except-Self/Solution_constant_space.java

---
+++ 238-Product-of-Array-Except-Self/Solution_constant_space.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] result = new int[len];
+        
+        result[0] = 1;
+        for (int i =  1; i < len; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int p = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            p = p * nums[i + 1];
+            result[i] *= p;
+        }
+        
+        return result;
+    }
+}
